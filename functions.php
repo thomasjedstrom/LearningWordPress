726bb53ed625ea4dfeac83589eb058da6d0f5d62
Thomas Edstrom <thomasjedstrom@gmail.com>
2016-10-25 23:18:24 -0500

    finished video 13

--- functions.php
+++ functions.php
@@ -45,6 +45,9 @@ function learningWordPress_setup(){
 	add_theme_support('post-thumbnails');
 	add_image_size('small-thumbnail', 180, 120, true);
 	add_image_size('banner-image', 920, 210, array('left', 'top'));
+
+	// Add post format support
+	add_theme_support('post-formats', array('aside', 'gallery', 'link'));
 }
 
 add_action('after_setup_theme', 'learningWordPress_setup');
--- single.php
+++ single.php
@@ -1,38 +1,26 @@
 <?php
 
-get_header();
+get_header(); ?>
 
-if (have_posts()) :
-	while (have_posts()) : the_post(); ?>
-
-	<article class="post">
-		<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-		<p class="post-info">
-			<?php the_time('F jS, Y g:i a'); ?> | by <a href="<?php echo get_author_posts_url(get_the_author_meta('ID')); ?>"><?php the_author(); ?></a> | Posted in
-
+	<div class="site-content clearfix">		
+		<div class="main-column">
 			<?php
-				$categories = get_the_category();
-				$separator = ", ";
-				$output = '';
-				if($categories){
-					foreach($categories as $category){
-						$output .= '<a href="' . get_category_link($category->term_id) . '">' . $category->cat_name . '</a>' . $separator;
-					}
-					echo trim($output, $separator);
-				}
-			?>
-		</p>
+				if (have_posts()) :
+					while (have_posts()) : the_post();
+						if (get_post_format() == false) {
+							get_template_part('content', 'single');
+						} else {
+							get_template_part('content', get_post_format());
+						}
+					endwhile;
+				else :
+					echo '<p>No content found</p>';
+				endif;
+			?>			
+		</div>
+		<?php get_sidebar(); ?>
+	</div>
 
-		<?php the_post_thumbnail('banner-image'); ?>
-
-		<?php the_content(); ?>
-	</article>
-
-	<?php endwhile;
-	else:
-		echo '<p>No content found</p>';
-	endif;
-
-get_footer();
+<?php get_footer();
 
 ?>
